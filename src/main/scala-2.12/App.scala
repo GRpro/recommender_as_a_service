8a5b6d4fef4a068d02d350a2b48abb22036da56f
Grigory Rozhkov <grigoriyroghkov@gmail.com>
2017-01-06 17:48:27 +0200

    Extract ZIP archives

--- src/main/scala-2.12/App.scala
+++ src/main/scala-2.12/App.scala
@@ -7,7 +7,20 @@ object App {
 
   def main(args: Array[String]): Unit = {
 
-    Dataset.loadResource(Dataset.smallDatasetUrl, Paths.get(Dataset.datasetsDirectory, Dataset.smallDatasetFileName))
-    Dataset.loadResource(Dataset.completeDatasetUrl, Paths.get(Dataset.datasetsDirectory, Dataset.completeDatasetFileName))
+
+    val datasetsDirectory = "datasets"
+
+    val smallDatasetFileName = "ml-latest-small.zip"
+    val smallDatasetUrl = s"http://files.grouplens.org/datasets/movielens/$smallDatasetFileName"
+
+    val completeDatasetFileName = "ml-latest.zip"
+    val completeDatasetUrl = s"http://files.grouplens.org/datasets/movielens/$completeDatasetFileName"
+
+//    val smallDatasetPath = Dataset.loadResource(smallDatasetUrl, Paths.get(datasetsDirectory, smallDatasetFileName))
+    val completeDatasetPath = Dataset.loadResource(completeDatasetUrl, Paths.get(datasetsDirectory, completeDatasetFileName))
+
+//    Dataset.unzip(smallDatasetPath, Paths.get(smallDatasetPath.getParent.toString, smallDatasetPath.getFileName.toString.substring(0, smallDatasetPath.getFileName.toString.lastIndexOf("."))))
+    Dataset.unzip(completeDatasetPath, Paths.get(datasetsDirectory))
+
   }
 }
--- src/main/scala-2.12/data/Dataset.scala
+++ src/main/scala-2.12/data/Dataset.scala
@@ -1,30 +1,45 @@
 package data
 
+import java.io.FileOutputStream
+
 import sys.process._
 import java.net.URL
 import java.nio.file.{Files, Path}
+import java.util.zip.ZipInputStream
 
 import com.typesafe.scalalogging._
 
 object Dataset extends LazyLogging {
-
-  val datasetsDirectory = "datasets"
-
-  val smallDatasetFileName = "ml-latest-small.zip"
-  val smallDatasetUrl = s"http://files.grouplens.org/datasets/movielens/$smallDatasetFileName"
-
-  val completeDatasetFileName = "ml-latest.zip"
-  val completeDatasetUrl = s"http://files.grouplens.org/datasets/movielens/$completeDatasetFileName"
 
   def loadResource(url: String, path: Path) = {
     if (!Files.exists(path)) {
       logger.info(s"$path does not exist. Creating.")
       path.getParent.toFile.mkdirs()
       path.toFile.createNewFile()
-      (new URL(smallDatasetUrl) #> path.toFile !!)
+      new URL(url).#>(path.toFile).!!
       logger.info(s"Resource $url was stored in $path")
     } else {
       logger.info(s"Resource $url already exists in $path")
+    }
+    path
+  }
+
+  def unzip(path: Path, destDir: Path): Unit = {
+    val zis = new ZipInputStream(Files.newInputStream(path))
+
+    Stream.continually(zis.getNextEntry).takeWhile(_ != null).foreach { file =>
+      if (!file.isDirectory) {
+        val outPath = destDir.resolve(file.getName)
+        val outPathParent = outPath.getParent
+        if (!outPathParent.toFile.exists()) {
+          outPathParent.toFile.mkdirs()
+        }
+
+        val outFile = outPath.toFile
+        val out = new FileOutputStream(outFile)
+        val buffer = new Array[Byte](4096)
+        Stream.continually(zis.read(buffer)).takeWhile(_ != -1).foreach(out.write(buffer, 0, _))
+      }
     }
   }
 }
