89c9d93e6e9f99cf84c378d4addf0b7a91ef796a
Grigory Rozhkov <grigoriyroghkov@gmail.com>
2017-03-10 19:00:04 +0200

    Add simple web UI

---
+++ web/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
+
+@app.route("/")
+def index():
+    return send_file("templates/index.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
