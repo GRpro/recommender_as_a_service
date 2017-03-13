3fd93419cecefe1cdb528fddb0d431b5a3f2590e
astepaniuk <astepaniuk@homeaway.com>
2017-03-13 22:52:35 +0200

    Refactoring MLTest into RatingService

---
+++ service/src/main/scala/gr/ml/analytics/service/RatingService.scala
@@ -0,0 +1,109 @@
+package gr.ml.analytics.service
+
+import java.io.File
+import java.nio.file.Paths
+
+import com.github.tototoshi.csv.CSVReader
+import gr.ml.analytics.model.SparkUtil
+import org.apache.spark.ml.recommendation.{ALS, ALSModel}
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.expressions.UserDefinedFunction
+import org.apache.spark.sql.functions.udf
+
+
+object RatingService{
+  val datasetsDirectory = "data"
+  val historicalRatingsPath = Paths.get(datasetsDirectory, "ml-latest-small", "ratings.csv").toAbsolutePath.toString
+  val currentRatingsPath = Paths.get(datasetsDirectory, "ml-latest-small", "current-ratings.csv").toAbsolutePath.toString
+  val predictionsPath = Paths.get(datasetsDirectory, "ml-latest-small", "predictions.csv").toAbsolutePath.toString
+  val modelPath = Paths.get(datasetsDirectory, "model").toAbsolutePath.toString
+  val bothRatingsPath = Paths.get(datasetsDirectory, "ml-latest-small").toAbsolutePath.toString + File.separator + "*ratings.csv"
+}
+
+class RatingService {
+  val toInt: UserDefinedFunction = udf[Int, String](_.toInt)
+  val toLong: UserDefinedFunction = udf[Long, String](_.toLong)
+  val toDouble: UserDefinedFunction = udf[Double, String](_.toDouble)
+
+  lazy val sparkSession = SparkUtil.sparkSession()
+  import sparkSession.implicits._
+
+  def persistRating(userId: Int, movieId: Int, rating: Double): Unit ={
+    import com.github.tototoshi.csv._
+    val writer = CSVWriter.open(RatingService.currentRatingsPath, append = true)
+    writer.writeRow(List(userId.toString, movieId.toString,rating.toString,(System.currentTimeMillis/1000).toString))
+  }
+
+  def persistPredictions(userId: Int, predictedMovieIds: java.util.List[Int]): Unit ={
+    import com.github.tototoshi.csv._
+    val writer = CSVWriter.open(RatingService.predictionsPath, append = true)
+    writer.writeRow(List(userId, predictedMovieIds.toArray.mkString(":")))
+  }
+  def loadPredictions(userId: Int, limit: Int) = {
+    val reader = CSVReader.open(RatingService.predictionsPath)
+    val filtered = reader.all().filter((pr:List[String])=>pr(0).toInt == userId)
+    val predictedMovieIdsFromFile = filtered.map((pr:List[String]) => pr(1).split(":").toList.map(m=>m.toInt)).last.take(limit)
+    predictedMovieIdsFromFile
+  }
+  def updatePredictionsForUser(userId: Int): java.util.List[Int] = {
+    val predictedMovieIds: java.util.List[Int] = calculatePredictedIdsForUser(userId, readModel())
+    persistPredictions(userId, predictedMovieIds)
+    predictedMovieIds
+  }
+
+  def calculatePredictedIdsForUser(userId: Int, model: ALSModel): java.util.List[Int] ={
+    val toRateDS: DataFrame = getMoviesNotRatedByUser(userId)
+    import org.apache.spark.sql.functions._
+    val predictions = model.transform(toRateDS).orderBy(col("prediction").desc)
+    val predictedMovieIds: java.util.List[Int] = predictions.select("movieId").map(row => row.getInt(0)).collectAsList()
+    predictedMovieIds
+  }
+
+  def getMoviesNotRatedByUser(userId: Int): DataFrame ={
+    val reader = CSVReader.open(RatingService.historicalRatingsPath) // TODO add support of several files, not just historical data
+    // TODO could we do it more effective that reading all the file at once? Would be still OK for larger files?
+    val movieIds = reader.all().filter((p:List[String])=>p(1)!="movieId" && p(0).toInt!=userId).map((p:List[String]) => p(1).toInt).toSet
+    val userMovieList: List[(Int, Int)] = movieIds.map(movieId => (userId, movieId)).toList
+    userMovieList.toDF("userId", "movieId")
+  }
+
+  def readModel(): ALSModel ={
+    ALSModel.load(RatingService.modelPath)
+  }
+
+  def writeModel(model: ALSModel): Unit ={
+    model.write.overwrite().save(RatingService.modelPath)
+  }
+
+  def updateModel(): Unit ={
+    val ratingsDF = loadRatings(RatingService.bothRatingsPath)
+
+    val als = new ALS()
+      .setMaxIter(5)
+      .setRegParam(0.01)
+      .setUserCol("userId")
+      .setItemCol("movieId")
+      .setRatingCol("rating")
+
+    val model = als.fit(ratingsDF)
+    writeModel(model)
+  }
+
+  def loadRatings(path: String): DataFrame ={
+    val ratingsDF = {
+      val ratingsStringDF = sparkSession.read
+        .format("com.databricks.spark.csv")
+        .option("header", "true")
+        .option("mode", "DROPMALFORMED")
+        .load(path) // TODO can we provide several different locations?
+        .select("userId", "movieId", "rating", "timestamp")
+
+      ratingsStringDF
+        .withColumn("userId", toInt(ratingsStringDF("userId")))
+        .withColumn("movieId", toInt(ratingsStringDF("movieId")))
+        .withColumn("rating", toDouble(ratingsStringDF("rating")))
+    }
+    ratingsDF
+  }
+
+}
--- service/src/test/scala/gr/ml/analytics/MLTest.scala
+++ service/src/test/scala/gr/ml/analytics/MLTest.scala
@@ -3,10 +3,10 @@ package gr.ml.analytics
 import java.io._
 import java.nio.file.Paths
 
-import com.github.tototoshi.csv.CSVReader
 import gr.ml.analytics.model._
+import gr.ml.analytics.service.RatingService
 import org.apache.spark.ml.recommendation.{ALS, ALSModel}
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{DataFrame, SaveMode}
 import org.apache.spark.sql.expressions.UserDefinedFunction
 import org.apache.spark.sql.functions.{isnan, not, udf}
 import org.scalatest._
@@ -19,22 +19,20 @@ class MLTest extends FlatSpec with BeforeAndAfterAllConfigMap{
   lazy val sparkSession = SparkUtil.sparkSession()
   import sparkSession.implicits._
 
-  var dataStore: DataStore = null
-  var predictionModelCreator: PredictionModelCreator = null
-  var predictionModel: PredictionModel = null
+  var ratingService: RatingService = new RatingService()
 
   val toInt: UserDefinedFunction = udf[Int, String](_.toInt)
   val toLong: UserDefinedFunction = udf[Long, String](_.toLong)
   val toDouble: UserDefinedFunction = udf[Double, String](_.toDouble)
 
-  val datasetsDirectory = "datasets"
+  val datasetsDirectory = "data"
   val smallDatasetFileName = "ml-latest-small.zip"
   val smallDatasetUrl = s"http://files.grouplens.org/datasets/movielens/$smallDatasetFileName"
   val historicalRatingsPath = Paths.get(datasetsDirectory, "ml-latest-small", "ratings.csv").toAbsolutePath.toString
   val currentRatingsPath = Paths.get(datasetsDirectory, "ml-latest-small", "current-ratings.csv").toAbsolutePath.toString
   val bothRatingsPath = Paths.get(datasetsDirectory, "ml-latest-small").toAbsolutePath.toString + File.separator + "*ratings.csv"
-
-  // TODO add automatical current-ratings file creation!
+  val predictionsPath = Paths.get(datasetsDirectory, "ml-latest-small", "predictions.csv").toAbsolutePath.toString
+  val modelPath = Paths.get(datasetsDirectory, "model").toAbsolutePath.toString
 
   override def beforeAll(configMap: ConfigMap): Unit = {
     Util.loadResource(smallDatasetUrl,
@@ -47,8 +45,16 @@ class MLTest extends FlatSpec with BeforeAndAfterAllConfigMap{
     writer.writeRow(List("userId", "movieId", "rating", "timestamp"))
   }
 
+  "DataFrame" should "be persisted without error" in {
+    val df: DataFrame = List(1,2,3).toDF("first")
+    val testDFPath = Paths.get(datasetsDirectory, "test", "df.out").toAbsolutePath.toString
+    df.write.mode(SaveMode.Overwrite).save(testDFPath)
+    val df2 = sparkSession.read.load(testDFPath)
+    println(df.collectAsList() == df2.collectAsList()); // TODO replace with assert
+  }
+
   "Persisted ALS model" should "give the same result" in {
-    val ratingsDF = loadRatings(historicalRatingsPath)
+    val ratingsDF = ratingService.loadRatings(historicalRatingsPath)
     val userId = 1 // just an existing user
 
     val als = new ALS()
@@ -59,37 +65,37 @@ class MLTest extends FlatSpec with BeforeAndAfterAllConfigMap{
       .setRatingCol("rating")
 
     val model = als.fit(ratingsDF)
-    writeModel(model)
-    val predictions1 = getPredictionsForUser(userId, model, 5)
-    val predictions2 = getPredictionsForUser(userId, readModel(), 5)
+    ratingService.writeModel(model)
+    val predictions1 = ratingService.calculatePredictedIdsForUser(userId, model)
+    val predictions2 = ratingService.calculatePredictedIdsForUser(userId, ratingService.readModel())
     // TODO replace with assert:
-    println(predictions1.collectAsList().equals(predictions2.collectAsList()))
+    println(predictions1.stream().limit(10).toArray.toList.equals(predictions2.stream().limit(10).toArray.toList))
   }
 
   "Whole prediction flow" should "run without errors" in {
     val userId: Int = 0 // new user
-    persistUserRating(userId, 1, 5.0)
-    persistUserRating(userId, 2, 4.0)
-    persistUserRating(userId, 3, 5.0)
-    persistUserRating(userId, 4, 4.0)
-    persistUserRating(userId, 5, 5.0)
-    persistUserRating(userId, 6, 3.0)
-    persistUserRating(userId, 7, 5.0)
-    persistUserRating(userId, 8, 4.0)
+    ratingService.persistRating(userId, 1, 5.0)
+    ratingService.persistRating(userId, 2, 4.0)
+    ratingService.persistRating(userId, 3, 5.0)
+    ratingService.persistRating(userId, 4, 4.0)
+    ratingService.persistRating(userId, 5, 5.0)
+    ratingService.persistRating(userId, 6, 3.0)
+    ratingService.persistRating(userId, 7, 5.0)
+    ratingService.persistRating(userId, 8, 4.0)
 
-    updateModel()
+    ratingService.updateModel()
 
-    val predictions = getPredictionsForUser(userId, readModel(), 5)
-    // TODO cache/save predictions
+    val predictedMovieIds = ratingService.updatePredictionsForUser(userId)
+    val predictedMovieIdsFromFile = ratingService.loadPredictions(userId, 5)
 
-    println("Predictions:")
-    predictions.show()
+    // TODO replace with assert
+    println(predictedMovieIds.toArray.toList.take(5).equals(predictedMovieIdsFromFile))
   }
 
 
   "Precision for historical data" should "should be reasonable" in {
 
-    val ratingsDF = loadRatings(historicalRatingsPath)
+    val ratingsDF = ratingService.loadRatings(historicalRatingsPath)
 
     val Array(training, test) = ratingsDF.randomSplit(Array(0.8, 0.2))
 
@@ -107,65 +113,5 @@ class MLTest extends FlatSpec with BeforeAndAfterAllConfigMap{
     val tp = predictions.filter($"rating" > 3.0 && $"prediction" > 3.0).count() // TODO VERY time-consuming, can we improve?
     val precision = tp.toFloat/total
     println(s"Precision = $precision") // approx. 0.5
-  }
-
-  def loadRatings(path: String): DataFrame ={
-    val ratingsDF = {
-      val ratingsStringDF = sparkSession.read
-        .format("com.databricks.spark.csv")
-        .option("header", "true")
-        .option("mode", "DROPMALFORMED")
-        .load(path) // TODO can we provide several different locations?
-        .select("userId", "movieId", "rating", "timestamp")
-
-      ratingsStringDF
-        .withColumn("userId", toInt(ratingsStringDF("userId")))
-        .withColumn("movieId", toInt(ratingsStringDF("movieId")))
-        .withColumn("rating", toDouble(ratingsStringDF("rating")))
-    }
-    ratingsDF
-  }
-
-  def getPredictionsForUser(userId: Int, model: ALSModel, number: Int): DataFrame ={
-    val toRateDS: DataFrame = getMoviesNotRatedByUser(userId)
-    import org.apache.spark.sql.functions._
-    val predictions = model.transform(toRateDS).orderBy(col("prediction").desc).limit(number);
-    predictions
-  }
-
-  def updateModel(): Unit ={
-    val ratingsDF = loadRatings(bothRatingsPath)
-
-    val als = new ALS()
-      .setMaxIter(5)
-      .setRegParam(0.01)
-      .setUserCol("userId")
-      .setItemCol("movieId")
-      .setRatingCol("rating")
-
-    val model = als.fit(ratingsDF)
-    writeModel(model)
-  }
-
-  def readModel(): ALSModel ={
-    ALSModel.load("model.dat")
-  }
-
-  def writeModel(model: ALSModel): Unit ={
-    model.write.overwrite().save("model.dat")
-  }
-
-  def persistUserRating(userId: Int, movieId: Int, rating: Double): Unit ={
-    import com.github.tototoshi.csv._
-    val writer = CSVWriter.open(currentRatingsPath, append = true)
-    writer.writeRow(List(userId.toString, movieId.toString,rating.toString,(System.currentTimeMillis/1000).toString))
-  }
-
-  def getMoviesNotRatedByUser(userId: Int): DataFrame ={
-    val reader = CSVReader.open(historicalRatingsPath) // TODO add support of several files, not just historical data
-    // TODO could we do it more effective that reading all the file at once? Would be still OK for larger files?
-    val movieIds = reader.all().filter((p:List[String])=>p(0)!=userId && p(1)!="movieId").map((p:List[String]) => p(1).toInt).toSet
-    val userMovieList: List[(Int, Int)] = movieIds.map(movieId => (userId, movieId)).toList
-    userMovieList.toDF("userId", "movieId")
   }
 }
