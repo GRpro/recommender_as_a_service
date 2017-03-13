8effd88ce1b3da64ff65fdd45f24fe6cfcc12d61
Grigory Rozhkov <grigoriyroghkov@gmail.com>
2017-03-13 17:58:12 +0200

    Finish draft implementation of UI on top of flask angular

--- service/src/main/scala/gr/ml/analytics/api/RecommendationServiceAPI.scala
+++ service/src/main/scala/gr/ml/analytics/api/RecommendationServiceAPI.scala
@@ -13,7 +13,7 @@ import spray.routing._
 /**
   * Spray REST API
   */
-class RecommendationServiceAPI extends Actor with HttpService with CORSSupport {
+class RecommendationServiceAPI extends Actor with HttpService {
 
   def actorRefFactory = context
 
@@ -28,37 +28,28 @@ class RecommendationServiceAPI extends Actor with HttpService with CORSSupport {
     /*
     Create new User Movie Rating relationships
     */
+    path("ratings") {
+      post {
+        entity(as[List[UserMovieRating]]) { ratings =>
 
-      respondWithHeaders(
-        RawHeader("Access-Control-Allow-Origin", "http://localhost:63342"),
-        RawHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS"),
-        RawHeader("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With")) {
-      path("ratings") {
-        post {
-          entity(as[List[UserMovieRating]]) { ratings =>
+          service.rateItems(ratings.map(userMovieRating =>
+            (userMovieRating.user, userMovieRating.movie, userMovieRating.rating)))
 
-            service.rateItems(ratings.map(userMovieRating =>
-              (userMovieRating.user, userMovieRating.movie, userMovieRating.rating)))
-
-            respondWithMediaType(MediaTypes.`application/json`) {
-              complete(StatusCodes.Created)
-            }
+          respondWithMediaType(MediaTypes.`application/json`) {
+            complete(StatusCodes.Created)
           }
         }
-      } ~ options {
-        complete(StatusCodes.OK)
-      }} ~ /*
+      }
+    } ~ /*
       Get top N movies for a particular user
        */
-      cors {
-        path("ratings" / LongNumber / "top" / IntNumber) { (userId, number) =>
-          get {
-            val topN = service.getTopNForUser(User(userId), number)
-            respondWithMediaType(MediaTypes.`application/json`) {
-              complete {
-                val converted = topN.map(movieRating => MovieRating(movieRating._1, movieRating._2))
-                converted
-              }
+      path("ratings" / LongNumber / "top" / IntNumber) { (userId, number) =>
+        get {
+          val topN = service.getTopNForUser(User(userId), number)
+          respondWithMediaType(MediaTypes.`application/json`) {
+            complete {
+              val converted = topN.map(movieRating => MovieRating(movieRating._1, movieRating._2))
+              converted
             }
           }
         }
@@ -73,54 +64,16 @@ class RecommendationServiceAPI extends Actor with HttpService with CORSSupport {
     Get N movies to be rated
     TODO make search APIs.
      */
-    cors {
-      path("movies" / IntNumber) { number =>
-        get {
-          respondWithMediaType(MediaTypes.`application/json`) {
-            val movies = service.getItems(number)
-            complete {
-              movies
-            }
+    path("movies" / IntNumber) { number =>
+      get {
+        respondWithMediaType(MediaTypes.`application/json`) {
+          val movies = service.getItems(number)
+          complete {
+            movies
           }
         }
       }
     }
-
   }
 
-}
-
-trait CORSDirectives { this: HttpService =>
-  private def respondWithCORSHeaders(origin: String) =
-    respondWithHeaders(
-      HttpHeaders.`Access-Control-Allow-Origin`(SomeOrigins(List(origin))),
-      HttpHeaders.`Access-Control-Allow-Credentials`(true),
-      HttpHeaders.`Access-Control-Allow-Methods`(HttpMethods.GET, HttpMethods.POST, HttpMethods.PUT, HttpMethods.DELETE, HttpMethods.OPTIONS),
-      HttpHeaders.`Access-Control-Allow-Headers`("Origin, X-Requested-With, Content-Type, Accept, Authorization")
-    )
-  private def respondWithCORSHeadersAllOrigins =
-    respondWithHeaders(
-      HttpHeaders.`Access-Control-Allow-Origin`(AllOrigins),
-      HttpHeaders.`Access-Control-Allow-Credentials`(true),
-      HttpHeaders.`Access-Control-Allow-Methods`(HttpMethods.GET, HttpMethods.POST, HttpMethods.PUT, HttpMethods.DELETE, HttpMethods.OPTIONS),
-      HttpHeaders.`Access-Control-Allow-Headers`("Origin, X-Requested-With, Content-Type, Accept, Authorization")
-    )
-
-  def corsFilter(origins: List[String])(route: Route) =
-    if (origins.contains("*"))
-//      respondWithCORSHeaders("http://localhost")
-      respondWithCORSHeadersAllOrigins(route)
-    else
-      optionalHeaderValueByName("Origin") {
-        case None =>
-          route
-        case Some(clientOrigin) => {
-          if (origins.contains(clientOrigin))
-            respondWithCORSHeaders(clientOrigin)(route)
-          else {
-            // Maybe, a Rejection will fit better
-            complete(StatusCodes.Forbidden, "Invalid origin")
-          }
-        }
-      }
 }
--- web/app.py
+++ web/app.py
@@ -13,18 +13,30 @@ def index():
 
 # REST Api
 
+
 @app.route('/ratings', methods=['POST'])
 def post_ratings():
-    body = str(request.data,'utf-8')
+    body = str(request.data, 'utf-8')
     headers = {'Content-type': 'application/json'}
     r = requests.post('http://localhost:8080/ratings', data=body, headers=headers)
     print(r.status_code)
     print(r.text)
     return 'OK'
 
-# @app.route('/movies/<n>', methods=['GET'])
-# def get_movies(n):
-#
+
+@app.route('/movies/<n>', methods=['GET'])
+def get_movies(n):
+    headers = {'Content-type': 'application/json'}
+    r = requests.get('http://localhost:8080/movies/%s' % n, headers=headers)
+    return r.content
+
+
+@app.route('/ratings/<user_id>/top/<n>', methods=['GET'])
+def get_top_ratings_for_user(user_id, n):
+    headers = {'Content-type': 'application/json'}
+    r = requests.get('http://localhost:8080/ratings/%s/top/%s' % (user_id, n), headers=headers)
+    return r.content
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8081)
+    app.run(host='0.0.0.0')
